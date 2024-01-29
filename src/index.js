ca187b4c996254ad9ba6df533563366a321a947e
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-29 17:57:21 +0530

    javaScript Expressions in JSX in React

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let flname = "Vinay Kumar";
+
 ReactDOM.render(
+
     <>
-    <h1>Vinay Kumar Netflix Pick</h1>
-    <h4>List of Top 5 series: </h4>
-    <ol>
-        <li>Dark</li>
-        <li>Extra Curricular</li>
-        <li>My Holo Love</li>
-        <li>My First-2 Love</li>
-        <li>Mr. Robot</li>
-    </ol>
+    <h1>My name is {flname}</h1>
+    <p>The sum of 2 + 6 = {Math.random()}</p>
     </>,
+
     document.getElementById("root"))
