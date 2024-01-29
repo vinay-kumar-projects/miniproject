cbef2564608d220ba6f06723b8654c24bfb2acfa
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-29 16:24:13 +0530

    Methods of Rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import ReactDom from 'react-dom'; 
+import ReactDOM from 'react-dom'; 
 
-ReactDom.render(<h1>Vinay Kumar</h1>, document.getElementById('root'))
+//First method of Rendering.
+
+ReactDOM.render(<h1>Deekonda</h1>, document.getElementById("root"))
+
+//Second method of Rendering by using babel. 
+
+ReactDOM.render(React.createElement("h1", null, "Vinay"), document.getElementById("root"))
+
+//Third method of Rendering by using pure JavaScript.
+
+var h1 = document.createElement("h1")
+h1.innerHTML = "Kumar";
+document.getElementById("root").appendChild(h1);
