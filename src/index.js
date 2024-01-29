7cb696e6262525af18dd0377d34a3a20543c7fd7
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-29 17:27:04 +0530

    Render multiple elements in ReactDOM.render() using fragment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 
 ReactDOM.render(
-    [
-    <h1>WelCome!!</h1>,
+    <React.Fragment>
+    <h1>WelCome!!</h1>
     <p>Hello My name is Vinay Kumar. Iam an IT student.</p>
-    ], 
+    </React.Fragment>, 
     document.getElementById("root"))
