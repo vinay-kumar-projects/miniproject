1e294da2020f95ac5d61b6abae8456824cd18cdb
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-29 16:33:01 +0530

    Render multiple elements in ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 
-//First method of Rendering.
-
-ReactDOM.render(<h1>Deekonda</h1>, document.getElementById("root"))
-
-//Second method of Rendering by using babel. 
-
-ReactDOM.render(React.createElement("h1", null, "Vinay"), document.getElementById("root"))
-
-//Third method of Rendering by using pure JavaScript.
-
-var h1 = document.createElement("h1")
-h1.innerHTML = "Kumar";
-document.getElementById("root").appendChild(h1);
+ReactDOM.render(
+    <div>
+    <h1>WelCome!!</h1>
+    <p>Hello My name is Vinay Kumar</p>
+    </div>, 
+    document.getElementById("root"))
