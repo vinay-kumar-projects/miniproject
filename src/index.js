a4ea5ae37289e857514a00901d521a4bcfed50e8
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-29 17:40:51 +0530

    Created a top 5 Netflix series list task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-    <React.Fragment>
-    <h1>WelCome!!</h1>
-    <p>Hello My name is Vinay Kumar. Iam an IT student.</p>
-    </React.Fragment>, 
+    <>
+    <h1>Vinay Kumar Netflix Pick</h1>
+    <h4>List of Top 5 series: </h4>
+    <ol>
+        <li>Dark</li>
+        <li>Extra Curricular</li>
+        <li>My Holo Love</li>
+        <li>My First-2 Love</li>
+        <li>Mr. Robot</li>
+    </ol>
+    </>,
     document.getElementById("root"))
