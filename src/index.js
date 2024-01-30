b8e0460bac820044337e431ce6a59383364b5f72
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-30 15:36:43 +0530

    JSX Attributes in ReactJS

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 
 const currDate = new Date().toLocaleDateString();
 const currTime = new Date().toLocaleTimeString();
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/200/300";
+const img3 = "https://picsum.photos/200/300";
+
 
 ReactDOM.render(
     <>
-    <h1>Vinay Kumar</h1>
-    <p>todays date is {currDate}</p>
-    <p>Current Time is {currTime}</p>
+    <h1 contentEditable = "true">Vinay Kumar</h1>
+    <a href='https://picsum.photos' target='_lorem'>
+    <img src = {img1} alt = "randomImages" />
+    <img src = {img2} alt = "randomImages" />
+    <img src = {img3} alt = "randomImages" />
+    </a>
     </>,
     document.getElementById("root")
 )
