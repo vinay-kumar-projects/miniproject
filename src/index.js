33ca80a97f77d2f5cb5193744c0b01272ad6f790
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-30 14:54:21 +0530

    ES6 Template literals in JSX in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let flname = "Vinay Kumar";
+let fname = "Vinay ";
+let mname = "Kumar ";
+let lname = ".D"
 
 ReactDOM.render(
 
     <>
-    <h1>My name is {flname}</h1>
-    <p>The sum of 2 + 6 = {Math.random()}</p>
+    <h1>My name is {`${fname} ${mname} ${lname}`}</h1>
     </>,
 
     document.getElementById("root"))
