c23323e9e43774f7c28ad0e052255ba30eaaa405
vinay-kumar-projects <vinayprojects@gmail.com>
2024-01-30 16:47:05 +0530

    Using Internal CSS and Inline CSS Styling in ReactJS

--- src/index.js
+++ src/index.js
@@ -8,10 +8,29 @@ const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/200/300";
 const img3 = "https://picsum.photos/200/300";
 
+// .heading {
+//     color: #fa9191;
+//     text-transform: capitalize;
+//     text-align: center;
+//     font-weight: bold;
+//     text-shadow: 0px, 2px, 4px, #ffe9cf;
+//     margin: 50px;
+//     font-family: 'Josefin Sans', sans-serif;
+  
+//   }
+
+const heading = {
+    color: "#fa9191",
+    textTransform: "capitalize",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "'Josefin Sans', sans-serif",
+    margin: "50px"
+}
 
 ReactDOM.render(
     <>
-    <h1 className = "heading" >Vinay Kumar</h1>
+    <h1 style={heading} >Vinay Kumar</h1>
     <div className='img_div'>
     <img src = {img1} alt = "randomImages" />
     <img src = {img2} alt = "randomImages" />
